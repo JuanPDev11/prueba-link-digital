e10e49ef8a2833facce272c70f948b4fb3929f32
Juan Pablo Lucumi Medina <110676660+JuanPDev11@users.noreply.github.com>
2023-07-22 13:56:20 -0500

    system of columns with Bootstrap finished

---
+++ js/menusec.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector(".columns__items");
+
+
+window.addEventListener('scroll', ()=>{menuOpacity()});
+
+function menuOpacity(){
+    let position = document.documentElement.scrollTop;
+    console.log(position);
+
+    if(position >= 245){
+        menu.classList.add("menu-op")
+    }
+    if(position < 245){
+        menu.classList.remove("menu-op")
+    }
+}
