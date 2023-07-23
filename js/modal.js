af96422c234b28b75e1587e6a4ec423f0df6fef9
Juan Pablo Lucumi Medina <110676660+JuanPDev11@users.noreply.github.com>
2023-07-23 18:41:49 -0500

    update modal

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+let close = document.querySelector(".modal1__close");
+let modal = document.querySelector(".modal1");
+
+close.addEventListener("click",()=>{
+    modal.classList.add("modal--show")
+})
