6f1edcccf7cac22aeca7e8f77ef832436673d924
Juan Pablo Lucumi Medina <110676660+JuanPDev11@users.noreply.github.com>
2023-07-26 11:59:59 -0500

    menu update

--- js/menusec.js
+++ js/menusec.js
@@ -6,18 +6,20 @@ window.addEventListener('scroll', ()=>{menuOpacity()});
 function menuOpacity(){
     let position = document.documentElement.scrollTop;
     let size = window.screen.width
-    //console.log(position);
+    console.log(size);
 
     //Mostrando y ocultando el menu secundario en tamaño estandar desktop
 
-    if(position >= 245 && position < 1400){
-        menu.classList.add("menu-op")
-    }
-    if(position < 245){
-        menu.classList.remove("menu-op")
-    }
-    if(position > 1400){
-        menu.classList.remove("menu-op")
+    if(size > 800){
+        if(position >= 245 && position < 1400){
+            menu.classList.add("menu-op")
+        }
+        if(position < 245){
+            menu.classList.remove("menu-op")
+        }
+        if(position > 1400){
+            menu.classList.remove("menu-op")
+        }
     }
 
     
